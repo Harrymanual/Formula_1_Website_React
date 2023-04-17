ab3fdfd70b9735492e0b492326a6f36c8f4b47a5
Harrymanual <89952205+Harrymanual@users.noreply.github.com>
2023-04-17 18:15:33 +1000

    index update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+
+// Use createRoot to create a root and render the app inside it
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );

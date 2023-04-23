28dad67cc2124c6d47e172bb54341635adb7e6c1
Harrymanual <89952205+Harrymanual@users.noreply.github.com>
2023-04-23 13:08:08 +1000

    Teams Page Major Update
    images work as buttons as intended, only remaining issue is css related which can be fixed at the end of the project

--- src/TeamsPage.js
+++ src/TeamsPage.js
@@ -1,26 +1,53 @@
-import backgroundImage from './images/team_logos_updated.png';
 import { useNavigate } from 'react-router-dom';
+import redbull from './images/redbull.jpg';
+import merc from './images/merrc.jpg';
+import sf from './images/sf.jpg';
+import aston from './images/aston.jpg';
+import renault from './images/renault.jpg';
+import alpha from './images/alpha.jpg';
+import alfa from './images/alfa.jpg';
+import williams from './images/williams.jpg';
+import mchonda from './images/mclaren.jpg';
+import haas from './images/haas.jpg';
+import './TeamsPage.css';
 
 
 const TeamsPage = () => {
   const navigate = useNavigate();
-  
+  const images = [
+    { src: redbull, alt: "Red Bull #mostawesometeam", path: "/page1" },
+    { src: merc, alt: "Merc #2ndbestteam", path: "/page2" },
+    { src: sf, alt: "rarri #whatcanisay #beststrategy", path: "/page3" },
+    { src: aston, alt: "aston #whyisstrollinthatrocket", path: "/page4" },
+    { src: renault, alt: "alpine #akabeerbudgetchampagne", path: "/page5" },
+    { src: alpha, alt: "alpha tauri #bteam", path: "/page6" },
+    { src: alfa, alt: "audi", path: "/page7" },
+    { src: williams, alt: "williams", path: "/page8" },
+    { src: mchonda, alt: "mchonda", path: "/page9" },
+    { src: haas, alt: "?", path: "/page10" },
+  ];
 
   return (
-    <div
+    <div className="teams-page"
       style={{
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundSize: 'cover',
         display: 'grid',
-        gridTemplateColumns: 'repeat(5, minmax(200px, 1fr))',
-        gridTemplateRows: 'repeat(2, minmax(0, 1fr))',
-        gap: '5px',
-        height: '100vh',
+        gridTemplateColumns: 'repeat(5, minmax(0, 1fr))',
+        gridAutoRows: '510px',
+        gap: '10px',
+        minHeight: '100vh',
         boxSizing: 'border-box',
+        padding: '10px',
       }}
     >
-      <h1>Teams Page</h1>
-
+      {images.map((image, index) => (
+        <button key={index} onClick={() => navigate(image.path)} style={{ backgroundColor: 'gray' }}>
+          <img
+            src={image.src}
+            alt={image.alt}
+            style={{ maxWidth: '100%', maxheight: '100%' }}
+          />
+        </button>
+      ))}
     </div>
   );
 };

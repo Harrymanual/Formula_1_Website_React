369a336e06a45484cc7a92ce65fc6cd9ce7494c9
Harrymanual <89952205+Harrymanual@users.noreply.github.com>
2023-05-27 14:03:19 +1000

    cool button protottype
    doesnt work yet gotta go though

---
+++ src/AboutButton.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './aboutButton.css';
+
+const colors = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#0000FF', '#4B0082', '#8B00FF']; // Colors of the rainbow
+
+const AboutButton = () => {
+  const [active, setActive] = useState(false);
+  const [colorIndex, setColorIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setColorIndex((colorIndex + 1) % colors.length);
+    }, 500);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [colorIndex]);
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    setActive(true);
+    setTimeout(() => {
+      setActive(false);
+      window.location.href = '/about';
+    }, 1000); 
+  };
+
+  const text = "About";
+  return (
+    <Link 
+      to="/about" 
+      className={`about-button ${active ? 'active' : ''}`} 
+      onClick={handleClick} 
+      style={{ color: colors[colorIndex] }}
+    >
+      {[...text].map((char, idx) => 
+        <span key={idx} className="about-letter">{char}</span>
+      )}
+    </Link>
+  );
+};
+
+export default AboutButton;

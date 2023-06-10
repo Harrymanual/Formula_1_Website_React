6bf0c32d8dd32347c7450c9afe1e110d7076b201
Harrymanual <89952205+Harrymanual@users.noreply.github.com>
2023-06-11 00:19:40 +1000

    jsut wanted to push something, dont listen to the about page yet

---
+++ src/Slides/modalText.js
@@ -0,0 +1,27 @@
+const modalTexts = [
+    [`Embarking on this expansive web development project was not a decision made lightly, yet it was one I knew I needed to undertake. The world of Formula 1 racing has always held a special place in my heart, its thrilling blend of high-speed competition, intricate design and strategy, and raw human talent creating a spectacle that's unparalleled. I wanted to channel this passion, the excitement and the pulsating energy of the racetrack, into a platform that could share the thrill of the sport with the world. I realized that the best way to understand something thoroughly is to dive into its depths, to break it down, build it up, and present it in a way that engages and educates others.
+
+    Indeed, the road is challenging. Web development is a complex domain, riddled with a myriad of problems that demand intricate solutions. However, the heart of this challenge also lies its appeal. It presents an opportunity not just to extend my knowledge of Formula 1, but to explore the intricacies of React, the nuances of Git, and the vast world of programming. The hurdles I face are high, but each one I overcome brings a sense of accomplishment and a deeper understanding of my craft.
+
+    This project, hence, is more than just an ambitious endeavor; it's a journey of personal growth and discovery. Through the arduous coding sessions and bouts of problem-solving, I'm not just building a website - I am building resilience, fortifying my skills, and creating something that resonates with my passion. I am weaving my love for Formula 1 with the craft of web development, translating my admiration for the sport into a digital experience. And that, in essence, is why I'm doing this - to blend passion with profession, to learn, to grow, and to create something I am truly proud of.`, 
+    
+    `beans
+    
+    double beans`, 
+    
+    'Modal text 3', 
+    'Modal text 4', 
+    'Modal text 5'],
+    ['Modal text 6', 
+    'Modal text 7', 
+    'Modal text 8', 
+    'Modal text 9', 
+    'Modal text 10'],
+    ['Modal text 11', 
+    'Modal text 12', 
+    'Modal text 13', 
+    'Modal text 14', 
+    'Modal text 15'],
+  ];
+  
+  export default modalTexts;
